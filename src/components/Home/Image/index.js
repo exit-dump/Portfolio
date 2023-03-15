326c23fa7522a6006e2a7eb71d70cb83bddec072
Akhil Jacob <rysetucker@gmail.com>
2023-03-16 00:41:47 +0530

    hope this time shit fires

--- src/components/Home/Image/index.js
+++ src/components/Home/Image/index.js
@@ -1,11 +1,11 @@
 import React from 'react'
-import LogoM from '/Windows/System32/react-portfolio/src/assets/images/logo-a8.png'
+import LogoM from '../../../assets/images/logo-a8.png'
 import './index.scss'
 
 function Image() {
   return (
     <div className='container'>
-      <img src={LogoM} alt="Random picture of coding/software development" className='img' />
+      <img src={LogoM} alt='programmer' className='img' />
     </div>
   )
 }
